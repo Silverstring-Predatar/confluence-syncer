d639c2de0f000cbe0315bbae6ab628e496bb3fad
humanendpoint <t.tunberg@catawiki.nl>
2023-10-23 23:33:06 +0200

    adding files

---
+++ README.md
@@ -0,0 +1,41 @@
+# Confluence Markdown Sync Action
+
+This Action copies the contents of a Markdown `.md` file or folder to a Confluence Cloud Page, with more features.
+
+## Getting Started
+
+```yml
+      - uses: humanendpoint/confluence-sync@v1
+        with:
+          input_md_directory: wiki
+          exclude_files: Home.md,_Sidebar.md
+          cloud: company
+          space_id: ${{ secrets.CONFLUENCE_SPACE_ID }}
+          parent_page_id: ${{ secrets.JIRA_SCRIPTS_PARENT_PAGE_ID }}
+          user: ${{ secrets.JIRA_USER }}
+          token: ${{ secrets.JIRA_TOKEN }}
+```
+
+## Authentication
+
+Uses a token for the rest api v2.
+
+## Input variables
+
+- `input_md_directory`: The folder you want to sync
+
+- `input_file`: If you don't want to sync a folder, you can also sync a single markdown file.
+
+*WARNING! ONLY ONE OF THE ABOVE!*
+
+- `exclude_files`: If there are files in the folder you would want to exclude
+
+- `cloud`: The ID can be found by looking at your confluence domain: `https://<cloud>.atlassian.net/...`
+
+- `space_id`: The space ID of the page
+
+- `parent_page_id`: The parent page ID of the page you want to crate a child page under
+
+- `user`: The user that generated the access token
+
+- `token`: You can generate the token [here](https://id.atlassian.com/manage-profile/security/api-tokens). Link to [Docs](https://confluence.atlassian.com/cloud/api-tokens-938839638.html)
---
+++ bin/main.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python
+"""
+This syncs markdown files from a file or folder to Confluence as child pages
+of a given parent, with their docstring, HTML formatted.
+It uses v2 of the Confluence API, and can exclude files.
+If selecting a single file, it assumes the file you want to sync is in the main repo dir.
+"""
+
+import os
+import requests
+from markdown_it import MarkdownIt
+from requests.exceptions import RequestException
+
+
+def load_environment_variables():
+    env_var_names = [
+        "cloud",
+        "user",
+        "token",
+        "parent_page_id",
+        "space_id",
+    ]
+
+    envs = {}
+    for var_name in env_var_names:
+        envs[var_name] = os.environ.get(var_name)
+        if not envs[var_name]:
+            print(f"Missing value for {var_name}")
+            exit(1)
+
+    if "input_file" in os.environ:
+        envs["input_file"] = os.environ["input_file"]
+    if "input_md_directory" in os.environ:
+        envs["input_md_directory"] = os.environ["input_md_directory"]
+    if "exclude_files" in os.environ:
+        envs["exclude_files"] = os.environ["exclude_files"]
+
+    return envs
+
+
+def process_directory(envs, links):
+    md_directory = os.path.join(
+        os.environ["GITHUB_WORKSPACE"], envs["input_md_directory"]
+    )
+    if envs["exclude_files"]:
+        exclude_files = envs["exclude_files"].split(",")
+
+    for root, _, files in os.walk(md_directory):
+        for f in files:
+            if exclude_files:
+                if f.endswith(".md") and f not in exclude_files:
+                    md_file = os.path.join(root, f)
+                    links = process_file(md_file, envs, links)
+            else:
+                if f.endswith(".md"):
+                    md_file = os.path.join(root, f)
+                    links = process_file(md_file, envs, links)
+
+    return links
+
+
+def process_file(md_file, envs, links):
+    new_version = None
+    try:
+        with open(md_file) as f:
+            md = f.read()
+
+        markdown = MarkdownIt()
+        # Render markdown files into HTML versions.
+        html = markdown.render(md)
+
+        page_title = os.path.splitext(os.path.basename(md_file))[0]
+        page_id, current_version, existing_content = find_page_by_title(page_title, envs)
+        headers = {"Content-Type": "application/json"}
+
+        if current_version:
+            new_version = current_version + 1
+
+        if page_id:
+            # compare existing content with the new content
+            if html != existing_content:
+                # content has changed, update it
+                update_url = f"https://{envs['cloud']}.atlassian.net/wiki/api/v2/pages/{page_id}"
+                update_content = {
+                    "id": page_id,
+                    "status": "current",
+                    "version": {"number": new_version},
+                    "title": page_title,
+                    "body": {"value": html, "representation": "storage"},
+                }
+
+                update_response = requests.put(
+                    update_url,
+                    json=update_content,
+                    auth=(envs["user"], envs["token"]),
+                    headers=headers,
+                )
+                if update_response.status_code == 200:
+                    updated_link = (
+                        f"https://{envs['cloud']}.atlassian.net/wiki"
+                        + update_response.json()["_links"]["webui"]
+                    )
+                    links.append(f"{page_title}: {updated_link}")
+                    print(f"{page_title}: Content update successful. New version: {new_version}")
+                else:
+                    print(
+                        f"{page_title}: Failed to update child page. HTTP status code: {update_response.status_code}"
+                    )
+            else:
+                # content is the same, no need to update
+                print(f"{page_title}: Identical content, no update required.")
+        else:
+            # construct the Confluence Rest API URL
+            url = f"https://{envs['cloud']}.atlassian.net/wiki/api/v2/pages"
+
+            # Confluence Rest API is weird, so spaceId is a hardcoded env var, same as parent page id.
+            content = {
+                "spaceId": envs["space_id"],
+                "status": "current",
+                "title": page_title,
+                "parentId": envs["parent_page_id"],
+                "body": {"value": html, "representation": "storage"},
+            }
+            headers = {"Accept": "application/json", "Content-Type": "application/json"}
+            response = requests.post(
+                url, json=content, auth=(envs["user"], envs["token"]), headers=headers
+            )
+
+            if response.status_code == 200:
+                link = (
+                    f"https://{envs['cloud']}.atlassian.net/wiki"
+                    + response.json()["_links"]["webui"]
+                )
+                links.append(f"{page_title}: {link}")
+                print(f"{page_title}: Content upload successful.")
+            else:
+                print(
+                    f"{page_title}: Failed to create child page. HTTP status code: {response.status_code}"
+                )
+    except RequestException as e:
+        print(f"An error occurred during the HTTP request: {e}")
+        exit(1)
+
+    if links:
+        return links
+
+
+def find_page_by_title(page_title, envs):
+    url = f"https://{envs['cloud']}.atlassian.net/wiki/api/v2/pages"
+    params = {
+        "title": page_title,
+    }
+    headers = {"Accept": "application/json"}
+    response = requests.get(
+        url, params=params, auth=(envs["user"], envs["token"]), headers=headers
+    )
+    if response.status_code == 200:
+        data = response.json()
+        if data.get("results"):
+            page_id = data["results"][0]["id"]
+            version_number = data["results"][0]["version"]["number"]
+            existing_content = data["results"][0]["body"]["storage"]
+            return page_id, version_number, existing_content
+
+    return None, None
+
+
+def main():
+    envs = load_environment_variables()
+
+    links = []
+    if "input_file" in envs:
+        # Process a single file
+        single_file = os.path.join(os.environ["GITHUB_WORKSPACE"], envs["input_file"])
+        links = process_file(single_file, envs, links)
+    elif "input_md_directory" in envs:
+        # Process a directory of markdown files
+        links = process_directory(envs, links)
+    else:
+        # Handle the case where neither are provided
+        print("No specific input provided.")
+        exit(1)
+
+    print(links)
+
+
+if __name__ == "__main__":
+    main()
